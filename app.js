30f6eebc986994811171e0f83053ee53dc714a86
tuanhuydev <tuanhuydev@gmail.com>
2023-01-27 22:06:38 +0700

    feat(common): setup app

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Hello AWS');
+const express = require("express");
+const path = require("path");
+
+// Make App
+const app = express();
+const port = 3000;
+
+// App setup
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Routing
+app.get('/', function(req, res) {
+    return res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+// Start app
+app.listen(port, function() {
+    console.log(`The app is running on port ${port}`);
+});
